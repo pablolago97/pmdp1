5a17fe1ae93b297cde41300bd19aa658fd4f71e6
pablolago97 <plago-bergonpazos@danielcastelao.org>
2017-04-02 19:26:07 +0200

    Segundo commit, metodo para invocat activity2 creado e interfaz de activity1 creada

--- app/src/main/java/com/example/pablomasterrace/fragments/MainActivity.java
+++ app/src/main/java/com/example/pablomasterrace/fragments/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.pablomasterrace.fragments;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int REQUEST_CODE = 1;
+    EditText texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        texto = (EditText)findViewById(R.id.texto);
+    }
+
+    //Metodo para invocar a la otra 'activity'
+    public void llamar_activity(View v){
+        Intent i = new Intent(this, Main2Activity.class);
+        i.putExtra("texto", texto.getText().toString());
+        startActivityForResult(i, REQUEST_CODE);
     }
 }
